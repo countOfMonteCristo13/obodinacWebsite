40858fe57caabdc0580929f911542a755f14e7ab
countOfMonteCristo13 <buzadzija.nikola13@gmail.com>
2023-11-08 22:31:13 +0100

    removing ts

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,61 @@
+// module.exports = {
+//   env: {
+//     browser: true,
+//     es2021: true,
+//   },
+//   extends: ['plugin:@typescript-eslint/recommended', 'airbnb', 'prettier'],
+//   parser: '@typescript-eslint/parser',
+//   parserOptions: {
+//     ecmaVersion: 2020,
+//     ecmaFeatures: {
+//       jsx: true,
+//     },
+//     sourceType: 'module',
+//   },
+//   settings: {
+//     react: {
+//       version: 'detect',
+//     },
+//     'import/resolver': {
+//       node: {
+//         extensions: ['.js', '.jsx', '.ts', '.tsx'],
+//         paths: ['./src'],
+//       },
+//     },
+//   },
+//   rules: {
+//     'no-use-before-define': 0,
+//     'import/extensions': 0,
+//     'import/no-unresolved': 0,
+//     'import/prefer-default-export': 0,
+//     'implicit-arrow-linebreak': 0,
+//     'react/jsx-filename-extension': [2, { extensions: ['.js', '.jsx', '.ts', '.tsx'] }],
+//     'arrow-body-style': 0,
+//     'react/prop-types': 0,
+//     'react/jsx-props-no-spreading': 0,
+//     'no-unused-vars': 0,
+//     '@typescript-eslint/explicit-module-boundary-types': 0,
+//     'no-extra-boolean-cast': 0,
+//     'no-shadow': 0,
+//     'react/button-has-type': 0,
+//     'jsx-a11y/anchor-is-valid': 0,
+//     'jsx-a11y/label-has-associated-control': 0,
+//     'jsx-a11y/click-events-have-key-events': 0,
+//     'jsx-a11y/no-static-element-interactions': 0,
+//     'react/function-component-definition': 'off',
+//     'react/no-unescaped-entities': 'off',
+//     'spaced-comment': 'off',
+//     'react/jsx-boolean-value': 'off',
+//     'no-nested-ternary': 'off',
+//   },
+// };
+
 module.exports = {
   env: {
     browser: true,
     es2021: true,
   },
-  extends: ['plugin:@typescript-eslint/recommended', 'airbnb', 'prettier'],
-  parser: '@typescript-eslint/parser',
+  extends: ['airbnb', 'prettier'],
   parserOptions: {
     ecmaVersion: 2020,
     ecmaFeatures: {
@@ -18,7 +69,7 @@ module.exports = {
     },
     'import/resolver': {
       node: {
-        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+        extensions: ['.js', '.jsx'],
         paths: ['./src'],
       },
     },
@@ -29,12 +80,11 @@ module.exports = {
     'import/no-unresolved': 0,
     'import/prefer-default-export': 0,
     'implicit-arrow-linebreak': 0,
-    'react/jsx-filename-extension': [2, { extensions: ['.js', '.jsx', '.ts', '.tsx'] }],
+    'react/jsx-filename-extension': [2, { extensions: ['.js', '.jsx'] }],
     'arrow-body-style': 0,
     'react/prop-types': 0,
     'react/jsx-props-no-spreading': 0,
     'no-unused-vars': 0,
-    '@typescript-eslint/explicit-module-boundary-types': 0,
     'no-extra-boolean-cast': 0,
     'no-shadow': 0,
     'react/button-has-type': 0,
