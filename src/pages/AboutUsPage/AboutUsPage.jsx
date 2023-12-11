c8dd868ed005d2d9c1cb25983cf4827ab6e007b7
countOfMonteCristo13 <buzadzija.nikola13@gmail.com>
2023-12-11 13:30:45 +0100

    resize grid

--- src/pages/AboutUsPage/AboutUsPage.jsx
+++ src/pages/AboutUsPage/AboutUsPage.jsx
@@ -12,28 +12,38 @@ const AboutUsPage = () => {
   }, []);
 
   return (
-    <div className='o__aboutPage slide-enterance1'>
+    <div className='slide-enterance1'>
       <NavbarSection activeLink='nav__about' />
-      <div className='o__aboutPage_section'>
-        <div className='o__aboutPage_section-img'>
-          <IKImage urlEndpoint={urlEndpoint} path={images.aboutUs} />
+      <section className='o__aboutPage-wrapper'>
+        <div className='o__aboutPage-headline'>
+          <div className='headline-question'>
+            <h2>Ko/Šta je Obodinac?</h2>
+          </div>
+          <div className='headline-answer'>
+            <p>
+              "Obodinac" je porodična firma koja je u ovom poslu već dugo godina. Osnovana je davne
+              1991. godine u Novom Sadu.
+            </p>
+          </div>
         </div>
-        <div className='o__aboutPage_section-description'>
-          <p>
-            "Obodinac" je porodična firma koja je u ovom poslu već dugo godina. Osnovana je davne
-            1991. godine u Novom Sadu.
-          </p>
-          <p>
-            Našim dugogodišnjim iskustvom, predanim radom, znanjem i trudom postali smo jedna od
-            vodećih firmi koja se bavi klimatizacijom, grejanjem i ventilacijom. Posedujemo
-            najsavremeniju opremu koja nam omogućava da kvalitetno, brzo i bezbedno obavimo posao.
-          </p>
-          <p>
-            Dokaz našeg kvalitetnog rada su naši zadovoljni stalni klijenti koji su naša najbolja
-            preporuka
-          </p>
+        <div className='o__aboutPage_section'>
+          <div className='o__aboutPage_section-img'>
+            <IKImage urlEndpoint={urlEndpoint} path={images.aboutUs} />
+          </div>
+          <div className='o__aboutPage_section-description'>
+            <h2>Klijenti na prvom mestu</h2>
+            <p>
+              Našim dugogodišnjim iskustvom, predanim radom, znanjem i trudom postali smo jedna od
+              vodećih firmi koja se bavi klimatizacijom, grejanjem i ventilacijom. Posedujemo
+              najsavremeniju opremu koja nam omogućava da kvalitetno, brzo i bezbedno obavimo posao.
+            </p>
+            <p>
+              Dokaz našeg kvalitetnog rada su naši zadovoljni stalni klijenti koji su naša najbolja
+              preporuka
+            </p>
+          </div>
         </div>
-      </div>
+      </section>
     </div>
   );
 };
