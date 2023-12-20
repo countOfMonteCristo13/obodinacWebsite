425901f3664040d2e3600bbfe6de5418e4b7d28d
countOfMonteCristo13 <buzadzija.nikola13@gmail.com>
2023-12-20 18:02:08 +0100

    updating mobile nav

--- src/components/index.js
+++ src/components/index.js
@@ -1,15 +1,14 @@
-import AboutUs from './AboutUs/AboutUs'
-import AdditionalOffers from './AdditionalOffers/AdditionalOffers'
-import Contact from './Contact/Contact'
-import FAQ from './FAQ/FAQ'
-import Footer from './Footer/Footer'
-import Gallery from './Gallery/Gallery'
-import Header from './Header/Header'
-import NavbarDropdown from './NavbarDropdown/NavbarDropdown'
-import NavbarMenuDropdown from './NavbarMenuDropdown/NavbarMenuDropdown'
-import NavbarSection from './NavbarSection/NavbarSection'
-import Offers from './Offers/Offers'
-import OffersSection from './OffersSection/OffersSection'
+import AboutUs from './AboutUs/AboutUs';
+import AdditionalOffers from './AdditionalOffers/AdditionalOffers';
+import Contact from './Contact/Contact';
+import FAQ from './FAQ/FAQ';
+import Footer from './Footer/Footer';
+import Gallery from './Gallery/Gallery';
+import Header from './Header/Header';
+import NavbarDropdown from './NavbarDropdown/NavbarDropdown';
+import NavbarSection from './NavbarSection/NavbarSection';
+import Offers from './Offers/Offers';
+import OffersSection from './OffersSection/OffersSection';
 
 export {
   AboutUs,
@@ -20,8 +19,7 @@ export {
   Gallery,
   Header,
   NavbarDropdown,
-  NavbarMenuDropdown,
   NavbarSection,
   Offers,
   OffersSection,
-}
+};
